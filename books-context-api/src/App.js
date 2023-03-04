72e12ce702189e0ff313fad9cd0a570332c5a110
Roddy <rratsirarson@dev-10.com>
2023-03-03 19:29:42 -0500

    created a json-server to host our books, then created CRUD functions to connect to it

--- books-context-api/src/App.js
+++ books-context-api/src/App.js
@@ -1,32 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
+import axios from 'axios';
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const deleteBookById =(id) => {
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    }
+
+    useEffect(() =>{
+        fetchBooks();
+    }, [])
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
 
         setBooks(updatedBooks);
     }
-    const handleCreateBook = (title) => {
+    const handleCreateBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
         const updatedBooks = [
-            ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title
-            }
+            ...books, response.data
         ];
+
         setBooks(updatedBooks);
+
+        // const updatedBooks = [
+        //     ...books,
+        //     {
+        //         id: Math.round(Math.random() * 9999),
+        //         title
+        //     }
+        // ];
+        // setBooks(updatedBooks);
+
     };
 
-    const changeOrder = (id, title) => {
+    const changeOrder = async (id, title) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: title,
+        });
+
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title};
+                return { ...book, ...response.data};
             }
 
             return book;
--- books-context-api/src/Components/BookList.js
+++ books-context-api/src/Components/BookList.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import BookShow from "./BookShow";
 
-function BookList({books, onDelete, changeOrder}) {
+function BookList({books, onDelete, changeOrder, bookState}) {
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} changeOrder={changeOrder} />
     });
 
     
     return (
+        
         <div className="book-list"> {renderedBooks} </div>
     )
 }
